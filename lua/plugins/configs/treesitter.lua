94db84b0c2e000ae6946c124c51cd63d07064ba8
Rivaldo Lawalata <nono.qwerty28@gmail.com>
2024-02-12 03:19:03 +0700

    first commit

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,16 @@
+local options = {
+  ensure_installed = { "lua", "javascript", "typescript", "tsx", "go", "css" },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+  --
+  -- autotag = {
+  --   enable = true
+  -- }
+}
+
+return options
